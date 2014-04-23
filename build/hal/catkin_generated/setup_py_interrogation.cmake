16ca7ef5d3ed62ce027ec939bb4ca632b783b47a
rss-student <rss-student@Turtle.mit.edu>
2014-04-23 13:38:19 -0400

    GCSERVO

---
+++ build/hal/catkin_generated/setup_py_interrogation.cmake
@@ -0,0 +1,6 @@
+set(hal_SETUP_PY_VERSION "0.0.0")
+set(hal_SETUP_PY_SCRIPTS "")
+set(hal_SETUP_PY_PACKAGES "hal")
+set(hal_SETUP_PY_PACKAGE_DIRS "src/hal")
+set(hal_SETUP_PY_MODULES "")
+set(hal_SETUP_PY_MODULE_DIRS "")
