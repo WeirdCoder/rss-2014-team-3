d3b680bd1097bcc01a9cf39e87f614754eae98ad
William Kalb <william@william-MacBookPro.(none)>
2014-04-25 16:25:55 -0400

    pathplanner and visual

--- src/robotbrain/src/pathplanner.py
+++ src/robotbrain/src/pathplanner.py
@@ -28,7 +28,7 @@ class PathPlanner(object):
         self.minY = minY; 
         self.maxY = maxY; 
         self.robotRadius = robotRadius;
-
+        
         self.cspace = [];
         self.createCSpace(startMap);
         self.createGraph();
@@ -50,26 +50,63 @@ class PathPlanner(object):
     # returns: none
     # updates CSpace with new obstacle
     def updateCSpace(self, obstacle):
-        self.cspace.append((self.makeCSObstacleFromCircle(obstacle, self.robotRadius));
-        return 
+        self.cspace.append(self.makeCSObstacleFromCircle(obstacle, self.robotRadius));
+        return
+                           
 
+#    def createGridGraph(self)
+#    x = (minY - minX)/
+#    self.gridGraph = networkx.grid_graph(dim=[x,y])
     # param: none
     # returns: none
     # makes grid of points that do not fall within cspace obstacles
     # turns grid into a graph
     def createGraph(self):
-        # TODO 
-  
-        pass
-
+        self.graph = networkx.graph()
+        for x in range(self.minX, self.maxX/self.gridDist):
+            for y in range(self.minY, self.maxY/self.gridDist):
+                graph.add_node((x*self.gridDist,y*self.gridDist))
+                if x > self.minX:
+                    graph.add_edge((x*self.gridDist,y*self.gridDist),((x-1)*self.gridDist,y*self.gridDist))
+                if y > self.minY:
+                    graph.add_edge((x*self.gridDist,y*self.gridDist),(x*self.gridDist,(y-1)*self.gridDist))
+           
+        for obs in self.cspace:
+            corners = obs.getLocationList()
+            edges = self.graph.edges()
+            for i in range(1,4):
+                x1 = corners[i-1].getX()
+                y1 = corners[i-1].getY()
+                x2 = corners[i%3].getX()
+                y2 = corners[i%3].getY()
+            for e in edges:
+                edgeX1 = e[0][0]
+                edgeY1 = e[0][1]
+                edgeX2 = e[1][0]
+                edgeY2 = e[1][1]
+                if intersects(edgeX1, edgeY1, edgeX2, edgeY2, x1, y1, x2, y2):
+                    self.graph.removeEdge((e[0],e[1]))
+                     
     # param: none
     # returns: none
     # makes grid of points that do not fall within cspace obstacles
     # turns grid into a graph
     def updateGraph(self, obstacle):
-        # TODO 
-        pass
-
+        corners = obstacle.getLocationList()
+        edges = self.graph.edges()
+        for i in range(1,4):
+            x1 = corners[i-1].getX()
+            y1 = corners[i-1].getY()
+            x2 = corners[i%3].getX()
+            y2 = corners[i%3].getY()
+        for e in edges:
+            edgeX1 = e[0][0]
+            edgeY1 = e[0][1]
+            edgeX2 = e[1][0]
+            edgeY2 = e[1][1]
+            if self.intersects(edgeX1, edgeY1, edgeX2, edgeY2, x1, y1, x2, y2):
+                self.graph.removeEdge((e[0],e[1]))
+       
     # param: startLocation: Location of start of path
     #        endLocation: Location of end of path
     # returns: List of Locations, including startLocation and endLocation
@@ -79,8 +116,42 @@ class PathPlanner(object):
     # runs through, smoothing
     def planPath(self, startLocation, endLocation):
         # TODO
-        pass
+        # Make this more efficient
+        startX = startLocation.getX()
+        startY = startLocation.getY()
+        endX = endLocation.getX()
+        endY = endLocation.getY()
+        nodes = self.graph.getNodes()
+        closestStartNode = nodes[0]
+        closestStartX = closestStartNode[0]
+        closestStartY = closestStartNode[1]
+        closestEndNode = nodes[0]
+        closestEndX = closestEndNode[0]
+        closestEndY = closestEndNode[1]
+        closestStartDist = self.getDistance(startX, startY, closestStartX, closestStartY)
+        closestEndDist = self.getDistance(endX, endY, closestEndX, closestEndY)
+        for node in nodes:
+            startDist = self.getDistance(node[0], node[1], closestStartX, closestStartY)
+            if startDist < closestStartDist:
+                closestStartNode = node
+                closestStartDist = startDist
+            endDist = self.getDistance(node[0], node[1], closestEndX, closestEndY)
+            if endDist < closestEndDist:
+                closestEndNode = node
+                closestEndDist = endDist
+
+        
+        
+        path = networkx.astar_path(self.graph, closestStartNode, closestEndNode, self.nodeDistance)
+        return path
+
+    def getDistance(self, x1, y1, x2, y2)
+        dist = math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2))
+        return dist
     
+    def nodeDistance(self, a, b)
+        dist = math.sqrt((a[0] - b[0])*(a[0] - b[0]) + (a[1] - b[1])*(a[1]-b[1]))
+        return dist
 ###################################
 # helper functions from lab5 code #
 ###################################
